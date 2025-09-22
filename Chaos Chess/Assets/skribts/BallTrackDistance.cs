63d154a67f8debac65a8cb6cc94d62198dd399db
Wupo958 <rebs.tobias@gmail.com>
2025-09-22 18:35:16 +0200

    baseball 2

--- Chaos Chess/Assets/skribts/BallTrackDistance.cs
+++ Chaos Chess/Assets/skribts/BallTrackDistance.cs
@@ -3,7 +3,6 @@ using XRMultiplayer.MiniGames;
 
 public class BallTrackDistance : MonoBehaviour
 {
-    private bool hit = false;
     private int playerNum;
     private float distance;
     private GameObject canvas;
@@ -18,26 +17,28 @@ public class BallTrackDistance : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 a = transform.position;
-        Vector3 b = bat.transform.position;
-        a.y = 0f;
-        b.y = 0f;
-        float distance = Vector3.Distance(a, b);
-
-        if (hit == true && transform.position.y > 1 && canvas.GetComponent<updateLeaderboard>().playerDist[playerNum] < distance)
+        if(bat != null)
         {
-            canvas.GetComponent<updateLeaderboard>().playerDist[playerNum] = distance;
-            miniGame_Baseball.SetDistance((int)distance);
+            Vector3 a = transform.position;
+            Vector3 b = bat.transform.position;
+            a.y = 0f;
+            b.y = 0f;
+            float distance = Vector3.Distance(a, b);
+
+            if (transform.position.y > 1 && canvas.GetComponent<updateLeaderboard>().playerDist[playerNum] < distance)
+            {
+                canvas.GetComponent<updateLeaderboard>().playerDist[playerNum] = distance;
+                miniGame_Baseball.SetDistance((int)distance);
+            }
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("bat") == true && hit == false)
+        if(collision.gameObject.CompareTag("bat") == true && bat == null)
         {
             Debug.Log("Ball hit");
             bat = collision.gameObject;
-            hit = true;
             canvas = collision.gameObject.GetComponent<BatPlayerNum>().canvas;
             playerNum = collision.gameObject.GetComponent<BatPlayerNum>().playerNum;
         }
