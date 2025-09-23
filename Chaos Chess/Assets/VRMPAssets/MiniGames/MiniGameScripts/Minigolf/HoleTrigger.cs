489a0f07f5d8f55d1da12d76f3a8567f2343d0e4
FRRRST <83558722+FRRRST@users.noreply.github.com>
2025-09-23 16:42:46 +0200

    Mini Golf fully polished

--- Chaos Chess/Assets/VRMPAssets/MiniGames/MiniGameScripts/Minigolf/HoleTrigger.cs
+++ Chaos Chess/Assets/VRMPAssets/MiniGames/MiniGameScripts/Minigolf/HoleTrigger.cs
@@ -12,20 +12,15 @@ public class HoleTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Make sure the object that entered is a golf ball
+
         if (other.CompareTag("GolfBall"))
         {
             GolfBall ball = other.GetComponentInParent<GolfBall>();
 
-            // IMPORTANT: Check if it's the LOCAL player's ball before submitting.
-            // Every player's game will detect the ball entering the trigger,
-            // but only the ball's owner should report it.
             if (ball.IsOwner)
             {
                 m_MiniGameGolf.PlayerHoledOut();
 
-                // Optional: Disable the ball's physics so it can't be knocked out.
-                // We'll also disable the trigger to prevent it from being called multiple times.
                 ball.GetComponent<Rigidbody>().isKinematic = true;
                 GetComponent<Collider>().enabled = false;
             }
